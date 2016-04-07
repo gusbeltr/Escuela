f21603c6453c1d27fe79629606628d2d6f5b7e18
Gustavo Beltran Luengo <gustavobl@lagash.com>
2016-04-07 11:56:13 -0300

    Crear proyecto polimorfismo

---
+++ Polimorfismo/Polimorfismo/Circle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo
+{
+    class Circle : Shape
+    {
+
+        public Circle(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+        public override void Draw()
+        {
+            Console.WriteLine("Dibujando un Circulo, Clase derivada");
+        }
+    }
+}
---
+++ Polimorfismo/Polimorfismo/Triangle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Polimorfismo
+{
+    internal class Triangle : Shape
+    {
+        public Triangle(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public override void Draw()
+        {
+            Console.WriteLine("Dibujando un triángulo");
+        }
+    }
+}
