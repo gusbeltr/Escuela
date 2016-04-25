53038d7fb1d771452b41b1456766f3ff6bbd645c
Gustavo Beltran Luengo <gustavobl@lagash.com>
2016-04-25 10:57:49 -0300

    Creando clase Folder

---
+++ TendenciaDat/TendenciaDat/Folder.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Linq;
+namespace TendenciaDat
+{
+    internal class Folder
+    {
+        private StringBuilder builder;
+        private IList<string> FilesPath { get; set; }
+        private List<FileInfo> Files { get; set; }
+        private string path { get; set; }
+
+        public Folder(string path)
+        {
+            this.path = path;
+            this.FilesPath = new List<string>();
+            this.Files = new List<FileInfo>();
+            this.builder = new StringBuilder();
+        }
+
+        public void GetPathName()
+        {
+            FilesPath = Directory.GetFiles(this.path);
+        }
+
+        public void BuilFileInfo()
+        {
+            foreach (string file in FilesPath)
+                Files.Add(new FileInfo(file));
+        }
+
+        public override string ToString()
+        {
+            builder.Clear();
+
+
+            for (int i = 0; i < Files.Count - 1; i++)
+                if (Files[i + 1] != null)
+                    builder.AppendLine(Files[i + 1].CreationTime.Subtract(Files[i].CreationTime).Seconds.ToString());
+
+            return builder.ToString();
+        }
+    }
+}
--- TendenciaDat/TendenciaDat/Program.cs
+++ TendenciaDat/TendenciaDat/Program.cs
@@ -1,39 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TendenciaDat
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            ProcessDirectory("C:\\temp");
+            var dir = new Folder("C:\\temp\\Boleta2");
+
+            dir.GetPathName();
+            dir.BuilFileInfo();
+            var output = dir.ToString();
+            Console.Write(output);
             Console.ReadKey();
         }
-
-        public static void ProcessDirectory(string targetDirectory)
-        {
-            var listFiles = new List<FileInfo>();
-            string[] fileEntries = Directory.GetFiles(targetDirectory);
-
-            foreach (string fileName in fileEntries)
-                listFiles.Add( new FileInfo(fileName));
-
-            var first = listFiles.OrderBy(f => f.CreationTime).FirstOrDefault();
-
-            foreach (var item in listFiles.OrderBy(f => f.CreationTime))
-            {
-                TimeSpan ts =item.CreationTime.Subtract(first.CreationTime);
-
-                Console.WriteLine(item.CreationTime+ "-" + first.CreationTime + "<====> " +ts.Seconds);
-
-                first = item;
-            }
-        }
-        
     }
 }
